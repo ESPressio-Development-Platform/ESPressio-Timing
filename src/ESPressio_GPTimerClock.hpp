515b5d198b4108941467b95dbf6b8fcaf455db39
Simon J Stuart <lakraven@gmail.com>
2026-08-27 18:09:12 +0200

    refactor(timing): remove esp_err_t from GPTimer compatibility clock API (#29)

--- src/ESPressio_GPTimerClock.hpp
+++ src/ESPressio_GPTimerClock.hpp
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 
+#include <ESPressio_Platform.hpp>
+
 #include "ESPressio_GPTimerTimeSource.hpp"
 #include "ESPressio_IStopwatchClock.hpp"
 #include "ESPressio_StopwatchClock.hpp"
@@ -9,196 +11,86 @@
 #if ESPRESSIO_TIMING_HAS_GPTIMER
 
 namespace ESPressio {
+namespace Timing {
 
-    namespace Timing {
+    template<
+        typename TTime = DefaultClockTime,
+        typename TLockPolicy = ThreadSafeLockPolicy,
+        typename TTick = ClockTick
+    >
+    class GPTimerClock : public IStopwatchClock<TTime> {
+    private:
+        GPTimerTimeSource _timeSource;
+        StopwatchClock<TTime, TLockPolicy, TTick> _stopwatch;
 
-        template<
-            typename TTime = DefaultClockTime,
-            typename TLockPolicy =
-                ThreadSafeLockPolicy,
-            typename TTick = ClockTick
-        >
-        class GPTimerClock :
-            public IStopwatchClock<
-                TTime
-            > {
+    public:
+        using TimeType = TTime;
+        using TickType = TTick;
 
-            private:
-                GPTimerTimeSource _timeSource;
+        explicit GPTimerClock(
+            bool startImmediately = false,
+            uint32_t requestedResolution =
+                ESPRESSIO_TIMING_GPTIMER_DEFAULT_RESOLUTION_HZ
+        )
+            : _timeSource(requestedResolution),
+              _stopwatch(
+                  startImmediately && _timeSource.GetIsAvailable(),
+                  &_timeSource
+              ) {}
 
-                StopwatchClock<
-                    TTime,
-                    TLockPolicy,
-                    TTick
-                > _stopwatch;
+        GPTimerClock(const GPTimerClock&) = delete;
+        GPTimerClock& operator=(const GPTimerClock&) = delete;
+        GPTimerClock(GPTimerClock&&) = delete;
+        GPTimerClock& operator=(GPTimerClock&&) = delete;
 
+        void Start() override {
+            if (_timeSource.GetIsAvailable()) _stopwatch.Start();
+        }
 
-            public:
-                using TimeType = TTime;
-                using TickType = TTick;
+        void Stop() override { _stopwatch.Stop(); }
+        void Reset() override { _stopwatch.Reset(); }
 
+        void Restart() override {
+            if (_timeSource.GetIsAvailable()) _stopwatch.Restart();
+        }
 
-                explicit GPTimerClock(
-                    bool startImmediately = false,
-                    uint32_t requestedResolution =
-                        ESPRESSIO_TIMING_GPTIMER_DEFAULT_RESOLUTION_HZ
-                )
-                    : _timeSource(
-                        requestedResolution
-                    ),
-                      _stopwatch(
-                          startImmediately &&
-                              _timeSource.
-                                  GetIsAvailable(),
-                          &_timeSource
-                      ) {
-                }
+        TTime GetTime() const override { return _stopwatch.GetTime(); }
+        TTime GetResolution() const override { return _stopwatch.GetResolution(); }
+        TTime GetLapTime() const override { return _stopwatch.GetLapTime(); }
 
+        bool GetIsRunning() const override {
+            return _timeSource.GetIsAvailable() && _stopwatch.GetIsRunning();
+        }
 
-                GPTimerClock(
-                    const GPTimerClock&
-                ) = delete;
+        bool GetIsAvailable() const { return _timeSource.GetIsAvailable(); }
 
-                GPTimerClock& operator=(
-                    const GPTimerClock&
-                ) = delete;
+        System::PlatformResult GetInitializationResult() const {
+            return _timeSource.GetInitializationResult();
+        }
 
-                GPTimerClock(
-                    GPTimerClock&&
-                ) = delete;
+        Observable::ObserverHandlePtr RegisterObserver(
+            IStopwatchClockObserver<TTime, TTick>* observer
+        ) {
+            return _stopwatch.RegisterObserver(observer);
+        }
 
-                GPTimerClock& operator=(
-                    GPTimerClock&&
-                ) = delete;
+        void UnregisterObserver(
+            IStopwatchClockObserver<TTime, TTick>* observer
+        ) {
+            _stopwatch.UnregisterObserver(observer);
+        }
 
+        ITimeSource* GetTimeSource() { return &_timeSource; }
 
-                void Start() override {
-                    if (
-                        _timeSource.
-                            GetIsAvailable()
-                    ) {
-                        _stopwatch.Start();
-                    }
-                }
+        void SetTime(const TTime& time) override {
+            _stopwatch.SetTime(time);
+        }
+    };
 
+    template<typename TTime = DefaultClockTime, typename TTick = ClockTick>
+    using SingleThreadedGPTimerClock = GPTimerClock<TTime, NoLockPolicy, TTick>;
 
-                void Stop() override {
-                    _stopwatch.Stop();
-                }
-
-
-                void Reset() override {
-                    _stopwatch.Reset();
-                }
-
-
-                void Restart() override {
-                    if (
-                        _timeSource.
-                            GetIsAvailable()
-                    ) {
-                        _stopwatch.Restart();
-                    }
-                }
-
-
-                TTime GetTime() const override {
-                    return
-                        _stopwatch.GetTime();
-                }
-
-
-                TTime GetResolution() const override {
-                    return
-                        _stopwatch.GetResolution();
-                }
-
-
-                TTime GetLapTime() const override {
-                    return
-                        _stopwatch.GetLapTime();
-                }
-
-
-                bool GetIsRunning() const override {
-                    return
-                        _timeSource.
-                            GetIsAvailable() &&
-                        _stopwatch.
-                            GetIsRunning();
-                }
-
-
-                bool GetIsAvailable() const {
-                    return
-                        _timeSource.
-                            GetIsAvailable();
-                }
-
-
-                esp_err_t
-                GetInitializationResult() const {
-                    return
-                        _timeSource.
-                            GetInitializationResult();
-                }
-
-
-                Observable::ObserverHandlePtr
-                RegisterObserver(
-                    IStopwatchClockObserver<
-                        TTime,
-                        TTick
-                    >* observer
-                ) {
-                    return
-                        _stopwatch.RegisterObserver(
-                            observer
-                        );
-                }
-
-
-                void UnregisterObserver(
-                    IStopwatchClockObserver<
-                        TTime,
-                        TTick
-                    >* observer
-                ) {
-                    _stopwatch.UnregisterObserver(
-                        observer
-                    );
-                }
-
-
-                ITimeSource*
-                GetTimeSource() {
-                    return &_timeSource;
-                }
-
-
-                void SetTime(
-                    const TTime& time
-                ) override {
-                    _stopwatch.SetTime(
-                        time
-                    );
-                }
-        };
-
-
-        template<
-            typename TTime = DefaultClockTime,
-            typename TTick = ClockTick
-        >
-        using SingleThreadedGPTimerClock =
-            GPTimerClock<
-                TTime,
-                NoLockPolicy,
-                TTick
-            >;
-
-    }
-
+}
 }
 
 #endif
