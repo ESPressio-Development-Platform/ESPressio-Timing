79b23a2addc5471c30f0552a165108ec9b2e3ddd
Simon J Stuart <lakraven@gmail.com>
2026-08-27 18:08:53 +0200

    refactor(timing): route default high-resolution time through System clock providers (#29)

--- src/ESPressio_TimeSource.hpp
+++ src/ESPressio_TimeSource.hpp
@@ -3,6 +3,8 @@
 #include <cstdint>
 #include <limits>
 
+#include <ESPressio_Clock.hpp>
+
 #include "ESPressio_ClockTypes.hpp"
 #include "ESPressio_ITimeSource.hpp"
 #include "ESPressio_GPTimerTimeSource.hpp"
@@ -13,306 +15,96 @@
     #define ESPRESSIO_TIMING_USE_GPTIMER_BY_DEFAULT 1
 #endif
 
-#if defined(ESP32)
-    #include <esp_timer.h>
-#elif defined(ARDUINO)
-    #include <Arduino.h>
-#else
-    #include <chrono>
-#endif
-
 namespace ESPressio {
+namespace Timing {
 
-    namespace Timing {
+    namespace Internal {
 
-        namespace Internal {
+        inline ClockTick TicksToNanoseconds(
+            uint64_t ticks,
+            uint64_t ticksPerSecond
+        ) {
+            if (ticksPerSecond == 0) return 0;
 
-            inline ClockTick TicksToNanoseconds(
-                uint64_t ticks,
-                uint64_t ticksPerSecond
-            ) {
-                if (ticksPerSecond == 0) {
-                    return 0;
-                }
+            const uint64_t wholeSeconds = ticks / ticksPerSecond;
+            const uint64_t remainingTicks = ticks % ticksPerSecond;
+            const uint64_t maximum = std::numeric_limits<uint64_t>::max();
 
-                const uint64_t wholeSeconds =
-                    ticks / ticksPerSecond;
+            if (wholeSeconds > maximum / NanosecondsPerSecond) return maximum;
 
-                const uint64_t remainingTicks =
-                    ticks % ticksPerSecond;
+            const uint64_t wholeNanoseconds = wholeSeconds * NanosecondsPerSecond;
+            const uint64_t remainingNanoseconds =
+                remainingTicks <= maximum / NanosecondsPerSecond
+                    ? (remainingTicks * NanosecondsPerSecond) / ticksPerSecond
+                    : static_cast<uint64_t>(
+                        (static_cast<long double>(remainingTicks) * NanosecondsPerSecond) /
+                        ticksPerSecond
+                      );
 
-                const uint64_t maximum =
-                    std::numeric_limits<
-                        uint64_t
-                    >::max();
-
-                if (
-                    wholeSeconds >
-                    maximum /
-                        NanosecondsPerSecond
-                ) {
-                    return maximum;
-                }
-
-                const uint64_t wholeNanoseconds =
-                    wholeSeconds *
-                    NanosecondsPerSecond;
-
-                const uint64_t remainingNanoseconds =
-                    remainingTicks <=
-                        maximum /
-                        NanosecondsPerSecond
-                        ? (
-                            remainingTicks *
-                            NanosecondsPerSecond
-                        ) /
-                            ticksPerSecond
-                        : static_cast<uint64_t>(
-                            (
-                                static_cast<long double>(
-                                    remainingTicks
-                                ) *
-                                NanosecondsPerSecond
-                            ) /
-                            ticksPerSecond
-                        );
-
-                if (
-                    remainingNanoseconds >
-                    maximum -
-                        wholeNanoseconds
-                ) {
-                    return maximum;
-                }
-
-                return
-                    wholeNanoseconds +
-                    remainingNanoseconds;
-            }
-
-
-            inline ClockTick GetSourceResolution(
-                uint64_t ticksPerSecond
-            ) {
-                if (ticksPerSecond == 0) {
-                    return 0;
-                }
-
-                return
-                    ticksPerSecond >=
-                        NanosecondsPerSecond
-                        ? 1
-                        : (
-                            NanosecondsPerSecond +
-                            ticksPerSecond -
-                            1
-                        ) /
-                            ticksPerSecond;
-            }
-
+            if (remainingNanoseconds > maximum - wholeNanoseconds) return maximum;
+            return wholeNanoseconds + remainingNanoseconds;
         }
 
-
-        template<typename TLockPolicy>
-        class HighResolutionTimeSourceT :
-            public ITimeSource {
-
-            #if ESPRESSIO_TIMING_HAS_GPTIMER && \
-                ESPRESSIO_TIMING_USE_GPTIMER_BY_DEFAULT
-
-            private:
-                GPTimerTimeSource _gptimerTimeSource;
-
-            #endif
-
-            #if defined(ARDUINO) && !defined(ESP32)
-
-            private:
-                mutable uint32_t _lastTick = 0;
-                mutable uint64_t _tickEpoch = 0;
-                mutable typename TLockPolicy::Mutex
-                    _tickMutex;
-
-            #endif
-
-            public:
-                uint64_t GetTicks() const override {
-                    #if defined(ESP32)
-
-                        #if ESPRESSIO_TIMING_HAS_GPTIMER && \
-                            ESPRESSIO_TIMING_USE_GPTIMER_BY_DEFAULT
-
-                            if (
-                                _gptimerTimeSource.
-                                    GetIsAvailable()
-                            ) {
-                                return
-                                    _gptimerTimeSource.
-                                        GetTicks();
-                            }
-
-                        #endif
-
-                        return static_cast<uint64_t>(
-                            esp_timer_get_time()
-                        );
-
-                    #elif defined(ARDUINO)
-
-                        const uint32_t currentTick =
-                            micros();
-
-                        typename TLockPolicy::Guard
-                            lock(_tickMutex);
-
-                        static constexpr uint64_t
-                            TickRange =
-                                1ULL << 32;
-
-                        static constexpr uint32_t
-                            HalfTickRange =
-                                1UL << 31;
-
-                        if (
-                            currentTick <
-                            _lastTick
-                        ) {
-                            const uint32_t difference =
-                                _lastTick -
-                                currentTick;
-
-                            if (
-                                difference >
-                                HalfTickRange
-                            ) {
-                                _tickEpoch +=
-                                    TickRange;
-
-                                _lastTick =
-                                    currentTick;
-                            }
-
-                            return
-                                _tickEpoch +
-                                currentTick;
-                        }
-
-                        if (
-                            currentTick -
-                                _lastTick >
-                                    HalfTickRange &&
-                            _tickEpoch >=
-                                TickRange
-                        ) {
-                            return
-                                (
-                                    _tickEpoch -
-                                    TickRange
-                                ) +
-                                currentTick;
-                        }
-
-                        _lastTick =
-                            currentTick;
-
-                        return
-                            _tickEpoch +
-                            currentTick;
-
-                    #else
-
-                        using SourceClock =
-                            std::chrono::
-                                steady_clock;
-
-                        return
-                            static_cast<uint64_t>(
-                                SourceClock::now().
-                                    time_since_epoch().
-                                    count()
-                            );
-
-                    #endif
-                }
-
-
-                uint64_t
-                GetTicksPerSecond() const override {
-                    #if defined(ESP32)
-
-                        #if ESPRESSIO_TIMING_HAS_GPTIMER && \
-                            ESPRESSIO_TIMING_USE_GPTIMER_BY_DEFAULT
-
-                            if (
-                                _gptimerTimeSource.
-                                    GetIsAvailable()
-                            ) {
-                                return
-                                    _gptimerTimeSource.
-                                        GetTicksPerSecond();
-                            }
-
-                        #endif
-
-                        return 1000000ULL;
-
-                    #elif defined(ARDUINO)
-
-                        return 1000000ULL;
-
-                    #else
-
-                        using SourceClock =
-                            std::chrono::
-                                steady_clock;
-
-                        return
-                            static_cast<uint64_t>(
-                                SourceClock::period::den /
-                                SourceClock::period::num
-                            );
-
-                    #endif
-                }
-
-
-                bool GetIsUsingGPTimer() const {
-                    #if ESPRESSIO_TIMING_HAS_GPTIMER && \
-                        ESPRESSIO_TIMING_USE_GPTIMER_BY_DEFAULT
-
-                        return
-                            _gptimerTimeSource.
-                                GetIsAvailable();
-
-                    #else
-
-                        return false;
-
-                    #endif
-                }
-
-
-                static HighResolutionTimeSourceT*
-                GetInstance() {
-                    static
-                        HighResolutionTimeSourceT
-                            instance;
-
-                    return &instance;
-                }
-        };
-
-
-        using HighResolutionTimeSource =
-            HighResolutionTimeSourceT<
-                ThreadSafeLockPolicy
-            >;
-
-        using SingleThreadedHighResolutionTimeSource =
-            HighResolutionTimeSourceT<
-                NoLockPolicy
-            >;
+        inline ClockTick GetSourceResolution(uint64_t ticksPerSecond) {
+            if (ticksPerSecond == 0) return 0;
+            return ticksPerSecond >= NanosecondsPerSecond
+                ? 1
+                : (NanosecondsPerSecond + ticksPerSecond - 1) / ticksPerSecond;
+        }
 
     }
 
+    template<typename TLockPolicy>
+    class HighResolutionTimeSourceT : public ITimeSource {
+    private:
+#if ESPRESSIO_TIMING_USE_GPTIMER_BY_DEFAULT
+        GPTimerTimeSource _highResolutionCounter;
+#endif
+
+    public:
+        uint64_t GetTicks() const override {
+#if ESPRESSIO_TIMING_USE_GPTIMER_BY_DEFAULT
+            if (_highResolutionCounter.GetIsAvailable()) {
+                return _highResolutionCounter.GetTicks();
+            }
+#endif
+            return System::Clock::Monotonic().NowNanoseconds();
+        }
+
+        uint64_t GetTicksPerSecond() const override {
+#if ESPRESSIO_TIMING_USE_GPTIMER_BY_DEFAULT
+            if (_highResolutionCounter.GetIsAvailable()) {
+                return _highResolutionCounter.GetTicksPerSecond();
+            }
+#endif
+            return NanosecondsPerSecond;
+        }
+
+        bool GetIsUsingHighResolutionCounter() const {
+#if ESPRESSIO_TIMING_USE_GPTIMER_BY_DEFAULT
+            return _highResolutionCounter.GetIsAvailable();
+#else
+            return false;
+#endif
+        }
+
+        // Compatibility name retained for callers from the previous ESP32-
+        // specific implementation. It no longer implies an ESP-IDF dependency.
+        bool GetIsUsingGPTimer() const {
+            return GetIsUsingHighResolutionCounter();
+        }
+
+        static HighResolutionTimeSourceT* GetInstance() {
+            static HighResolutionTimeSourceT instance;
+            return &instance;
+        }
+    };
+
+    using HighResolutionTimeSource =
+        HighResolutionTimeSourceT<ThreadSafeLockPolicy>;
+
+    using SingleThreadedHighResolutionTimeSource =
+        HighResolutionTimeSourceT<NoLockPolicy>;
+
+}
 }
